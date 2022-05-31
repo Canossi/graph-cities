499259acfa857898826f4ef48864e4ff3a9754de
mario_canossi@hotmail.com <canossi.moc@pf.gov.br>
2022-05-31 15:34:57 -0300

    first commit

---
+++ graph.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from collections import deque
+
+df = pd.read_csv('example.csv')
+
+class City:
+    def __init__(self, name):
+        self.name = name
+
+class Graph:
+    def __init__(self):
+        pass
+
+def a_star(start, goal, h):
+    pass
+
+
+
+
+
+def greedy_search():
+    pass
+
+
+
+def depth_search():
+    pass
+
